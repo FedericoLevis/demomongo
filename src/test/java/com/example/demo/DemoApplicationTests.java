8ec90fe87b71db86c2b630b3b22caa9639fa18a6
LYNX\federico.levis <federico.levis@lynxcom.com>
2024-02-15 18:10:56 +0100

    Test Commit 5

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /*
-Classe per iTest
+Classe per i Test automatici
  */
 
 @SpringBootTest
